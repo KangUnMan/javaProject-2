26e264ba54d85c5e03054894abfe34768bdd463d
Unyeong Kang <dnsdud426@naver.com>
2023-04-19 13:19:16 +0900

    feat: CrawlDate

---
+++ src/main/java/org/techit/CrawlDate.java
@@ -0,0 +1,12 @@
+package org.techit;
+
+import java.time.LocalDateTime;
+
+public class CrawlDate {
+    private LocalDateTime lastCrawlDateTime;
+
+    public CrawlDate(LocalDateTime lastCrawlDateTime) {
+        this.lastCrawlDateTime = lastCrawlDateTime;
+    }
+    //alt+Enter
+}
