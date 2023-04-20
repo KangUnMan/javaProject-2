2ad0db9c9ec6a1d51f3f152f66598f8f0182f720
Unyeong Kang <dnsdud426@naver.com>
2023-04-20 11:49:57 +0900

    fix: printf로 날짜 출력 예시

--- src/main/java/org/techit/week1/PrintYearMonthDay.java
+++ src/main/java/org/techit/week1/PrintYearMonthDay.java
@@ -3,7 +3,9 @@ package org.techit.week1;
 
 public class PrintYearMonthDay {
     public static void main(String[] args) {
-
+        System.out.printf("%04d\n",2013);
+        System.out.printf("%02d\n",4);
+        System.out.printf("%02d\n",3);
         System.out.printf("%04d.%02d.%02d",2014,4,7);
     }
 }
