d13336db5bec415ad9649d5cfe15c5b7198afb1b
Unyeong Kang <dnsdud426@naver.com>
2023-04-20 10:37:31 +0900

    fix: 안내글 작성하기

--- src/main/java/org/techit/week1/ScannerEx.java
+++ src/main/java/org/techit/week1/ScannerEx.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class ScannerEx {
     public void plusNumbers(){
         Scanner sc = new Scanner(System.in);
-        System.out.println(sc.next()+sc.next());
+        System.out.println("두개의 숫자를 입력하세요. 입력하고 엔터 입력하고 엔터");
+        System.out.print("첫번째숫자:");
+        System.out.println("첫번째 숫자는 "+sc.nextInt()+"입니다");
+        System.out.print("두번째숫자:");
+        System.out.println("두번째 숫자는 "+sc.nextInt()+"입니다");
     }
 }
