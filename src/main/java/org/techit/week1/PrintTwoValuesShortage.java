cede00abbe9010468dac3b2498878ba1d9a82268
Unyeong Kang <dnsdud426@naver.com>
2023-04-20 11:28:24 +0900

    feat: printf사용 숙지

---
+++ src/main/java/org/techit/week1/PrintTwoValuesShortage.java
@@ -0,0 +1,7 @@
+package org.techit.week1;
+
+public class PrintTwoValuesShortage {
+    public static void main(String[] args) {
+        System.out.printf("%d * %d = %d",2,2,4);
+    }
+}
---
+++ src/main/java/org/techit/week1/PrintY.java
@@ -0,0 +1,12 @@
+package org.techit.week1;
+
+public class PrintY {
+    void print(){
+        System.out.printf("%s","Why");
+    }
+
+    public static void main(String[] args) {
+        PrintY printY = new PrintY();
+        printY.print();
+    }
+}
