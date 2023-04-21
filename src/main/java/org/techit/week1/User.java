2d44157793aeea9d63c91fd7d50addbd97eaba43
Unyeong Kang <dnsdud426@naver.com>
2023-04-21 15:14:29 +0900

    feat: 유저 오브젝트 작성

---
+++ src/main/java/org/techit/week1/User.java
@@ -0,0 +1,8 @@
+package org.techit.week1;
+
+public class User {
+    //이름 , 전화번호 , 나이
+    String name;
+    String phoneNumber;
+    int age;
+}
---
+++ src/main/java/org/techit/week1/UserTest.java
@@ -0,0 +1,15 @@
+package org.techit.week1;
+
+public class UserTest {
+    public static void main(String[] args) {
+        User user = new User();
+        user.name = "김경록";
+        user.phoneNumber = "010-1111-1212";
+        user.age = 37;
+
+        User jaesung = new User();
+        jaesung.name = "재성 매니저님";
+        jaesung.phoneNumber = "010-0000-1235";
+        jaesung.age = 24;
+    }
+}
