8c358a7e7e63d015d7cac227ad08cae2587c55d8
Unyeong Kang <dnsdud426@naver.com>
2023-04-25 14:28:24 +0900

    feat : 배열선언, 값넣기,출력

---
+++ src/main/java/org/techit/week2/ArrayEx.java
@@ -0,0 +1,17 @@
+package org.techit.week2;
+
+import java.util.Arrays;
+
+public class ArrayEx {
+    public static void main(String[] args) {
+        int[] iArr;
+        iArr = new int[5];
+        System.out.println(iArr[0]);
+        System.out.println(iArr[1]);
+        System.out.println(iArr[2]);
+
+        int[] iArr2;
+        iArr2 = new int[] {3,5,7,2,1};
+        System.out.println(Arrays.toString(iArr2));
+    }
+}
---
+++ src/main/java/org/techit/week2/ArraysToString.java
@@ -0,0 +1,11 @@
+package org.techit.week2;
+
+import java.util.Arrays;
+
+public class ArraysToString {
+    public static void main(String[] args) {
+        int[] iArr;
+        iArr = new int[5];
+        System.out.println(Arrays.toString(iArr));
+    }
+}
