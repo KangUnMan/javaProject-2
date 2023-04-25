b001c4729f2ab266edbb0b31fc715c61b121ac6b
Unyeong Kang <dnsdud426@naver.com>
2023-04-25 09:16:16 +0900

    feat : 코드업 65번 fix: 64번

--- src/main/java/org/techit/codeup/CodeUp64.java
+++ src/main/java/org/techit/codeup/CodeUp64.java
@@ -9,8 +9,9 @@ public class CodeUp64 {
         int greaterThanNum2 = sc.nextInt();
         int greaterThanNum3 = sc.nextInt();
 
-        System.out.printf("%d",(greaterThanNum1<greaterThanNum2 ? greaterThanNum1:greaterThanNum2)<greaterThanNum3 ?
-                (greaterThanNum1<greaterThanNum2 ? greaterThanNum1:greaterThanNum2):greaterThanNum3 );
+        System.out.printf("%d", (greaterThanNum1 < greaterThanNum2 ? greaterThanNum1 : greaterThanNum2) < greaterThanNum3 ?
+                (greaterThanNum1 < greaterThanNum2 ? greaterThanNum1 : greaterThanNum2) : greaterThanNum3);
         // 변수 1과 2를 비교하여 변수 1이 2보다 작으면 1반환 아니면 2반환 그둘보다 3이 작으면 3반환
         // 변수1보다 2가 작으면 2반환 같을시? 3반환인듯함
+    }
 }
---
+++ src/main/java/org/techit/codeup/CodeUp65.java
@@ -0,0 +1,22 @@
+package org.techit.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp65 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numEven1 = sc.nextInt();
+        int numEven2 = sc.nextInt();
+        int numEven3 = sc.nextInt();
+        if(numEven1%2==0){
+            System.out.printf("%d\n",numEven1);
+        }
+        if(numEven2%2==0){
+            System.out.printf("%d\n",numEven2);
+        }
+        if(numEven3%2==0){
+            System.out.printf("%d\n",numEven3);
+        }
+
+    }
+}
