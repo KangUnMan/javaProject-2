bdbe2f90494f8afb3579116219a89710b040c56b
Unyeong Kang <dnsdud426@naver.com>
2023-04-26 14:11:25 +0900

    feat : 산술연산자 / 와 % 활용

---
+++ src/main/java/org/techit/week2/QuotientRemainderCalculator.java
@@ -0,0 +1,9 @@
+package org.techit.week2;
+
+public class QuotientRemainderCalculator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+        System.out.printf("%d와 %d의 나누기의 결과 : 몫 : %d , 나머지 %d",a,b,a/b,a%b);
+    }
+}
