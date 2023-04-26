593eae04e9619683902d867a6649a8f5b6e3401e
Unyeong Kang <dnsdud426@naver.com>
2023-04-26 10:41:35 +0900

    feat : TenxTenForEx - 반복문으로 코드 간소화

---
+++ src/main/java/org/techit/week2/TenxTenForEx.java
@@ -0,0 +1,14 @@
+package org.techit.week2;
+
+import java.util.Arrays;
+
+public class TenxTenForEx {
+    public static void main(String[] args) {
+        int[][] arr10x10 = new int[10][10];
+
+        for (int i=0; i < 10; i++){ //반복문
+            System.out.println(Arrays.toString(arr10x10[i]));
+        }
+
+    }
+}
