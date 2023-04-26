8d1a452f8ff4019cb48f5d7ff6003ba2816392e7
Unyeong Kang <dnsdud426@naver.com>
2023-04-26 13:17:01 +0900

    fix : 행 넣어서 바꾸기

--- src/main/java/org/techit/week2/TwoDimArrFillROw.java
+++ src/main/java/org/techit/week2/TwoDimArrFillROw.java
@@ -4,21 +4,23 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class TwoDimArrFillROw {
-    public static void inputRowArr(int[][] arrRow){
-        Scanner scan = new Scanner(System.in);
-        int row = scan.nextInt();
+    public static void inputRowArr(int[][] arrRow , int row){
         for (int i=0; i<arrRow.length; i++){
             arrRow[row][i]=1;
         }
     }
+
+    public static void printArray(int[][] arrRow ){
+        for (int i=0; i<arrRow.length; i++){
+            System.out.println(Arrays.toString(arrRow[i])); // 행 단위로 출력
+        }
+    }
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         int[][] arr = new int[5][5];
-        inputRowArr(arr);
-        System.out.println(Arrays.toString(arr[0])); // 행 단위로 출력
-        System.out.println(Arrays.toString(arr[1]));
-        System.out.println(Arrays.toString(arr[2]));
-        System.out.println(Arrays.toString(arr[3]));
-        System.out.println(Arrays.toString(arr[0]));
+        int row = scan.nextInt();
+        inputRowArr(arr,row);
+        printArray(arr);
     }
 
 }
