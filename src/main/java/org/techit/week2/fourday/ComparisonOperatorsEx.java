1bc368c50a2d61548fb3379adf8499c58e1b40c4
Unyeong Kang <dnsdud426@naver.com>
2023-04-27 10:39:23 +0900

    feat : 비교 연산자 활용 연습

---
+++ src/main/java/org/techit/week2/fourday/ComparisonOperatorsEx.java
@@ -0,0 +1,18 @@
+package org.techit.week2.fourday;
+
+public class ComparisonOperatorsEx {
+    public static void main(String[] args) {
+        int iVal1 = 10;
+        int iVal2 = 20;
+        boolean result ;
+
+        System.out.printf("iVal1 == iVal2 --> %b\n",iVal1==iVal2);
+        System.out.printf("iVal1 != iVal2 --> %b\n",iVal1!=iVal2);
+        System.out.printf("iVal1 < iVal2 --> %b\n",iVal1<iVal2);
+        System.out.printf("iVal1 <= iVal2 --> %b\n",iVal1<=iVal2);
+        System.out.printf("iVal1 > iVal2 --> %b\n",iVal1>iVal2);
+        System.out.printf("iVal1 >= iVal2 --> %b\n",iVal1>=iVal2);
+
+
+    }
+}
