dd2b117ff4cee3f73c2b6f383249e2f4eb51c2e7
Unyeong Kang <dnsdud426@naver.com>
2023-04-27 11:32:19 +0900

    feat : 논리연산자 활용 연습

---
+++ src/main/java/org/techit/week2/fourday/LogicalOperatorEx.java
@@ -0,0 +1,12 @@
+package org.techit.week2.fourday;
+
+public class LogicalOperatorEx {
+    public static void main(String[] args) {
+        int age = 25;
+        boolean isStudent = false;
+
+        if(age >= 18 && !isStudent){
+            System.out.printf("성인이며 학생이 아닙니다.");
+        }
+    }
+}
