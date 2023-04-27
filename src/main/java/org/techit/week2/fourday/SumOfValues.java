7c61222858ce1cec099e99857cd7b5955f460139
Unyeong Kang <dnsdud426@naver.com>
2023-04-27 09:24:52 +0900

    feat : 배열에 있는 값 누적해서 더하기.

---
+++ src/main/java/org/techit/week2/fourday/SumOfValues.java
@@ -0,0 +1,14 @@
+package org.techit.week2.fourday;
+
+public class SumOfValues {
+    public static void main(String[] args) {
+        int[] arr = {2, 1, 7, 4};
+        int answer = 0;
+        answer = answer + arr[0];
+        answer = answer + arr[1];
+        answer = answer + arr[2];
+        answer = answer + arr[3];
+
+        System.out.println(answer);
+    }
+}
