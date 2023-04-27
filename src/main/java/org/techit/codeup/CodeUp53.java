629e0a3b80ecbfe35c30a1797ed1535b1c7fef64
Unyeong Kang <dnsdud426@naver.com>
2023-04-27 13:19:23 +0900

    fix : 코드업 53~58번

--- src/main/java/org/techit/codeup/CodeUp53.java
+++ src/main/java/org/techit/codeup/CodeUp53.java
@@ -6,7 +6,7 @@ public class CodeUp53 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int zeroTF = scan.nextInt();
-        boolean zeroTrue = zeroTF==0;
+        boolean zeroTrue = zeroTF == 0;
         System.out.printf("%d",zeroTrue ? 1 : 0);
     }
 }
--- src/main/java/org/techit/codeup/CodeUp57.java
+++ src/main/java/org/techit/codeup/CodeUp57.java
@@ -5,11 +5,10 @@ import java.util.Scanner;
 public class CodeUp57 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        //자바에서도 입력을 받을때 공백으로 받으면 ex)1 3 ) a가 1을 받고 b가 3을 받을 수 있다는 사실을 지금 알았다..
         int allTrueCheckVar1 = scan.nextInt();
         int allTrueCheckVar2 = scan.nextInt();
 
-        boolean allTrueCheckbool = allTrueCheckVar1 ==allTrueCheckVar2; // 두개가 서로 같을 경우
+        boolean allTrueCheckbool = allTrueCheckVar1 == allTrueCheckVar2; // 두개가 서로 같을 경우
 
         System.out.printf("%d",allTrueCheckbool ? 1 : 0);
 
