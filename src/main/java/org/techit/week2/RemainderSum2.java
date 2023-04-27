6d6612c79b90a10c18c302368d4a2658854f907b
Unyeong Kang <dnsdud426@naver.com>
2023-04-27 09:12:08 +0900

    fix: 자릿수 더하기

--- src/main/java/org/techit/week2/RemainderSum2.java
+++ src/main/java/org/techit/week2/RemainderSum2.java
@@ -4,15 +4,17 @@ public class RemainderSum2 {
     public static void main(String[] args) {
         int num = 687;
         int answer = 0;
+        //무조건 나머지를 먼저 구하고 나누기를 해야함.
         answer = answer + num % 10;
         num = num / 10;
         System.out.printf("answer: %d\n",answer);
-
+        //7
         answer = answer + num % 10;
         num = num / 10;
         System.out.printf("answer: %d\n",answer);
-
+        //15
         answer = answer + num % 10;
         System.out.printf("answer: %d\n",answer);
+        //21
     }
 }
