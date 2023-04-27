8113e2e296984c2255ff40add16050825f539ef2
Unyeong Kang <dnsdud426@naver.com>
2023-04-27 15:24:45 +0900

    fix : 코드업 63~64번

--- src/main/java/org/techit/codeup/CodeUp63.java
+++ src/main/java/org/techit/codeup/CodeUp63.java
@@ -7,7 +7,7 @@ public class CodeUp63 {
         Scanner sc = new Scanner(System.in);
         int greaterThanNum1 = sc.nextInt();
         int greaterThanNum2 = sc.nextInt();
-
+//
         System.out.printf("%d",greaterThanNum1>greaterThanNum2 ? greaterThanNum1:greaterThanNum2);
     }
 }
--- src/main/java/org/techit/codeup/CodeUp64.java
+++ src/main/java/org/techit/codeup/CodeUp64.java
@@ -8,7 +8,7 @@ public class CodeUp64 {
         int greaterThanNum1 = sc.nextInt();
         int greaterThanNum2 = sc.nextInt();
         int greaterThanNum3 = sc.nextInt();
-
+//
         System.out.printf("%d", (greaterThanNum1 < greaterThanNum2 ? greaterThanNum1 : greaterThanNum2) < greaterThanNum3 ?
                 (greaterThanNum1 < greaterThanNum2 ? greaterThanNum1 : greaterThanNum2) : greaterThanNum3);
         // 변수 1과 2를 비교하여 변수 1이 2보다 작으면 1반환 아니면 2반환 그둘보다 3이 작으면 3반환
