e43a64fd90090629d6724485c6f6ecbc25af911b
Unyeong Kang <dnsdud426@naver.com>
2023-05-01 13:25:48 +0900

    feat : For문 a~z까지 반복

---
+++ src/main/java/org/techit/week3/day1/ForLoopAlpabatEx.java
@@ -0,0 +1,9 @@
+package org.techit.week3.day1;
+
+public class ForLoopAlpabatEx {
+    public static void main(String[] args) {
+        for(char c = 'a'; c<='z'; c++){
+            System.out.printf("%c ",c);
+        }
+    }
+}
