63fee8ecf5b52a83edd953aaf04781567414c920
Unyeong Kang <dnsdud426@naver.com>
2023-05-01 09:09:00 +0900

    feat : 코드업 1103,1106

---
+++ src/main/java/org/techit/codeUp3week/CodeUp1103.java
@@ -0,0 +1,7 @@
+package org.techit.codeUp3week;
+
+public class CodeUp1103 {
+    public static void main(String[] args) {
+        System.out.printf("\"c:\\test\"");
+    }
+}
---
+++ src/main/java/org/techit/codeUp3week/CodeUp1106.java
@@ -0,0 +1,9 @@
+package org.techit.codeUp3week;
+
+public class CodeUp1106 {
+    public static void main(String[] args) {
+        int max = Integer.MAX_VALUE;
+        int min = Integer.MIN_VALUE;
+        System.out.printf("%d %d",min,max);
+    }
+}
