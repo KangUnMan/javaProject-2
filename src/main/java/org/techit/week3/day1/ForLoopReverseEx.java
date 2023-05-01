d3705a992c80c0682ad605498ee28ea35b6b4fff
Unyeong Kang <dnsdud426@naver.com>
2023-05-01 11:28:41 +0900

    feat : For 문 거꾸로 활용 연습

---
+++ src/main/java/org/techit/week3/day1/ForLoopReverseEx.java
@@ -0,0 +1,9 @@
+package org.techit.week3.day1;
+
+public class ForLoopReverseEx {
+    public static void main(String[] args) {
+        for (int i=10; i>=0; i--){
+            System.out.printf("%d ",i);
+        }
+    }
+}
