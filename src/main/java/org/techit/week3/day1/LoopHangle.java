9319e2ee9710e250efc8287aa3c70e714b923861
Unyeong Kang <dnsdud426@naver.com>
2023-05-01 13:42:21 +0900

    feat : 한글 루프

---
+++ src/main/java/org/techit/week3/day1/LoopHangle.java
@@ -0,0 +1,9 @@
+package org.techit.week3.day1;
+
+public class LoopHangle {
+    public static void main(String[] args) {
+        for(char c ='ㄱ'; c<='ㅌ'; c++){
+            System.out.printf("%c ",c);
+        }
+    }
+}
