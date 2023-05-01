810526364b94eb4a42868500303dc5d6b4e4ef0f
Unyeong Kang <dnsdud426@naver.com>
2023-05-01 14:59:58 +0900

    feat : 코드업 98번

--- src/main/java/org/techit/codeup/CodeUp98.java
+++ src/main/java/org/techit/codeup/CodeUp98.java
@@ -8,15 +8,31 @@ public class CodeUp98 {
         //첫 줄에 격자판의 세로(h), 가로(w) 가 공백을 두고 입력되고,
         //두 번째 줄에 놓을 수 있는 막대의 개수(n)
         //세 번째 줄부터 각 막대의 길이(l), 방향(d), 좌표(x, y)가 입력된다.
-        int wide = sc.nextInt(); // 판의 넓이
+        //코드 입력받는 위치를 잘 보자.
         int hei = sc.nextInt(); // 판의 높이
+        int wide = sc.nextInt(); // 판의 넓이
         int[][]pan = new int[hei][wide]; //판 크기
         int stick = sc.nextInt(); // 막대의 개수
 
-        for(int i=0; i<stick; i++){ // 막대의 개수 만큼 반복.
+        for(int i=1; i<=stick; i++){ // 막대의 개수 만큼 반복.
             int leng = sc.nextInt(); // 막대의 길이
             int dir =  sc.nextInt(); // 막대의 방향 0이면 가로 1이면 세로
-           // if()
+            int xPoint = sc.nextInt(); // x위치
+            int yPoint = sc.nextInt(); // y위치
+
+            for (int j=0; j<leng; j++) {
+                if (dir == 0) // 가로
+                    pan[xPoint-1][yPoint-1+j] = 1;
+                else
+                    pan[xPoint-1+j][yPoint-1] = 1;
+            }
+        }
+
+        for(int l=0 ; l<hei; l++){
+            for (int m=0; m<wide; m++){
+                System.out.printf("%d ",pan[l][m]);
+            }
+            System.out.println();
         }
 
     }
