3034f7c8eb622f373a3bc9961cf4f6baf143d47a
Unyeong Kang <dnsdud426@naver.com>
2023-05-01 13:13:59 +0900

    feat : 코드업 73~75번

---
+++ src/main/java/org/techit/week3/day1/ForLoopFowardStep.java
@@ -0,0 +1,15 @@
+package org.techit.week3.day1;
+
+public class ForLoopFowardStep {
+    public static void main(String[] args) {
+        for(int i=0; i<10;  i += 2){
+            System.out.printf("%d ",i);
+        }
+
+        System.out.println();
+
+        for(int i=1; i<10;  i += 2){
+            System.out.printf("%d ",i);
+        }
+    }
+}
