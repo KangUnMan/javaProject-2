2600986ee0815253d5742fccea1ca6d13b28dfe4
Unyeong Kang <dnsdud426@naver.com>
2023-05-02 11:03:52 +0900

    feat : 코드업 83번

--- src/main/java/org/techit/codeup/CodeUp83.java
+++ src/main/java/org/techit/codeup/CodeUp83.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class CodeUp83 {
     public static void main(String[] args) {
+        // 3 ,6 ,9 게임
         Scanner sc = new Scanner(System.in);
         int threeSixNine = sc.nextInt();
         for(int i=1; i<=threeSixNine; i++){
