ebd32c9423f8819eee459b16b0767456da29b842
Unyeong Kang <dnsdud426@naver.com>
2023-05-03 13:23:40 +0900

    fix : = 추가

--- src/main/java/org/techit/week3/day3/IsPrimeDivide2.java
+++ src/main/java/org/techit/week3/day3/IsPrimeDivide2.java
@@ -8,7 +8,7 @@ public class IsPrimeDivide2 {
 
         int num = sc.nextInt();
         int factors = 0;
-        for(int i =2; i<num/2; i++){
+        for(int i =2; i<=num/2; i++){
             if(num%i==0){
                 factors++;
             }
--- src/main/java/org/techit/week3/day3/IsPrimeRoot.java
+++ src/main/java/org/techit/week3/day3/IsPrimeRoot.java
@@ -8,7 +8,7 @@ public class IsPrimeRoot {
 
         int num = sc.nextInt();
         int factors = 0;
-        for(int i =2; i * i < num; i++){
+        for(int i =2; i * i <= num; i++){
             if(num%i==0){
                 factors++;
             }
