425b4c2a10e409a250e41a47acb77f508c22b341
Unyeong Kang <dnsdud426@naver.com>
2023-05-03 13:20:04 +0900

    feat : 소수 구하기 /2 까지 검색 루트 n이하 검색

---
+++ src/main/java/org/techit/week3/day3/IsPrimeRoot.java
@@ -0,0 +1,18 @@
+package org.techit.week3.day3;
+
+import java.util.Scanner;
+
+public class IsPrimeRoot {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int factors = 0;
+        for(int i =2; i * i < num; i++){
+            if(num%i==0){
+                factors++;
+            }
+        }
+        System.out.printf("%s",factors==0 ? "prime":"not prime" );
+    }
+}
