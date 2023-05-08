95d7040ac6c66816936ca1e79f5621df0af9f531
Unyeong Kang <dnsdud426@naver.com>
2023-05-08 15:50:27 +0900

    feat : String[] args 출력

--- src/main/java/org/techit/week4/day1/repeatRPC.java
+++ src/main/java/org/techit/week4/day1/repeatRPC.java
@@ -28,16 +28,22 @@ public class repeatRPC {
         Random random = new Random();
         Scanner sc = new Scanner(System.in);
 
-        int player;
-        int computer;
+        int player = 0;
+        String rps;
+        int computer =0;
         do {
-            System.out.println("가위 바위 보 게임을 시작하겠습니다. 이길때까지 반복합니다. 0 바위 1 가위 2보");
-            player = sc.nextInt();
+            System.out.println("가위 바위 보 게임을 시작하겠습니다. 이길때까지 반복합니다.");
+            rps = sc.nextLine();
+            switch (rps){
+                case "바위"-> player = 0;
+                case "가위"-> player = 1;
+                case "보"-> player = 2;
+            }
             computer = random.nextInt(3);
             if(rock.winnerPlayer(player,computer)){
-                System.out.printf("승리하였습니다. 플레이어 :%d 컴퓨터: %d\n",player,computer);
+                System.out.printf("승리하였습니다.\n",player,computer);
             }else{
-                System.out.printf("승리하지못했습니다. 플레이어 :%d 컴퓨터: %d\n",player,computer);
+                System.out.printf("승리하지못했습니다.\n",player,computer);
             }
         }while (!rock.winnerPlayer(player,computer));
 
