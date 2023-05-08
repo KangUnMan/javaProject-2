ff2ee9046a3ae9150f5b65925094d3e16e43bd8d
Unyeong Kang <dnsdud426@naver.com>
2023-05-08 13:24:04 +0900

    feat : 메소드 활용 연습 , VariousReturnType

--- src/main/java/org/techit/week4/day1/PyramidStar.java
+++ src/main/java/org/techit/week4/day1/PyramidStar.java
@@ -4,7 +4,7 @@ public class PyramidStar {
     public static void main(String[] args) {
         int height = 3;
         for (int i = 0; i < height; i++) { //전체 Root
-            for (int j = 0; j < height - i - 1; j++) {
+            for (int j = 0; j < height - i - 1; j++) { // 3 2 1
                 System.out.printf(" ");
             }
             for (int j = 0; j < 2 * i + 1; j++) {
---
+++ src/main/java/org/techit/week4/day1/VariousReturnType.java
@@ -0,0 +1,27 @@
+package org.techit.week4.day1;
+
+public class VariousReturnType {
+    public boolean isAdult(){
+        return true;
+    }
+
+    public int plus(){
+        return 1+1;
+    }
+
+    public User getUser(){
+        return new User();
+    }
+
+    public void PrintHello(){
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+        VariousReturnType vrt = new VariousReturnType();
+        boolean isAdult = vrt.isAdult();
+        int plusResult = vrt.plus();
+        User user = vrt.getUser();
+        vrt.PrintHello();
+    }
+}
