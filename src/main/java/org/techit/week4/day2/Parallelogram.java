74961a860bc709614269c58c83cd452b5eea197a
Unyeong Kang <dnsdud426@naver.com>
2023-05-09 10:40:47 +0900

    feat : 평행사변형

--- src/main/java/org/techit/week4/day2/Parallelogram.java
+++ src/main/java/org/techit/week4/day2/Parallelogram.java
@@ -1,32 +1,25 @@
 package org.techit.week4.day2;
 
-public class ReversePyramid {
+public class Parallelogram {
 
-    public ReversePyramid(String spaceChar) {
+    public Parallelogram(String spaceChar) {
         this.spaceChar = spaceChar;
     }
 
     private String spaceChar = "0";
 
-    public String formatReversePyramid(int height, int i){
-        return String.format("%s%s\n",spaceChar.repeat(height-i),
-                "*".repeat(2*i-1));
-    }
-
     public String formatParlleogram(int height ,int i){
         return String.format("%s%s\n",spaceChar.repeat(height-i-1),"*".repeat(height));
     }
 
-    public void printReversePyramid(int height){
+    public void printParrlegram(int height){
         for(int i=0; i<height; i++){
             System.out.print(formatParlleogram(height,i));
         }
     }
     public static void main(String[] args) {
-
-        ReversePyramid reversePyramid = new ReversePyramid(" ");
-        reversePyramid.printReversePyramid(5);
-
+        Parallelogram parallelogram = new Parallelogram("0");
+        parallelogram.printParrlegram(4);
 
     }
 }
