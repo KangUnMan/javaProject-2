bb019549f157da1d7394334261e53293fad0c0c0
Unyeong Kang <dnsdud426@naver.com>
2023-05-09 09:48:27 +0900

    feat : 피라미드 생성자로 분리

--- src/main/java/org/techit/week4/day2/Pyramid2.java
+++ src/main/java/org/techit/week4/day2/Pyramid2.java
@@ -6,22 +6,32 @@ public class Pyramid2 {
     }
 
     private String spaceChar = "0";
-    public void pyramidPrint(int height){
-        for(int i = 0; i < height; i++){
-            for(int j= 0; j < height - i - 1; j++){
-                System.out.printf("%s",spaceChar);
+
+    public void pyramidPrint(int height) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < height - i - 1; j++) {
+                System.out.printf("%s", spaceChar);
             }
-            for(int j= 0; j < 2 * i - 1; j++){
+            for (int j = 0; j < 2 * i - 1; j++) {
                 System.out.printf("*");
             }
             System.out.println();
         }
     }
+
+    public String makeALine(int height, int i) {
+        return String.format("%s%s\n", spaceChar.repeat(height - i - 1), "*".repeat(2 * i + 1));
+    }
+
+    public void  printPyramidChar(int height){
+        for(int i=0; i<height; i++){
+            System.out.print(makeALine(height, i));
+        }
+    }
+
     public static void main(String[] args) {
         int height = 4;
         Pyramid2 pyramid2 = new Pyramid2("1");
-        pyramid2.pyramidPrint(4);
-        Pyramid2 pyramid3 = new Pyramid2("0");
-        pyramid3.pyramidPrint(6);
+        pyramid2.printPyramidChar(2);
     }
 }
--- src/main/java/org/techit/week4/day2/ReversePyramid.java
+++ src/main/java/org/techit/week4/day2/ReversePyramid.java
@@ -1,7 +1,29 @@
 package org.techit.week4.day2;
 
 public class ReversePyramid {
+
+    public ReversePyramid(String spaceChar) {
+        this.spaceChar = spaceChar;
+    }
+
+    private String spaceChar = "0";
+    public void printReversePyramid(int height){
+        for(int i=0; i<height; i++){
+            for(int j = 0; j < i ; j++){
+                System.out.printf("%s",spaceChar);
+            }
+            for(int j = 0; j < height+2-i*2; j++){
+                System.out.printf("*");
+            }
+
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
+
+        ReversePyramid reversePyramid = new ReversePyramid(" ");
+        reversePyramid.printReversePyramid(5);
+
 
     }
 }
