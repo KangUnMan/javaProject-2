9207a370bebc26c1f44046ee414d9ec2bb2005c3
Unyeong Kang <dnsdud426@naver.com>
2023-05-09 13:09:50 +0900

    feat : 구구단 리펙토링

--- src/main/java/org/techit/week4/day2/MultiplicationTable2To4.java
+++ src/main/java/org/techit/week4/day2/MultiplicationTable2To4.java
@@ -1,19 +1,24 @@
 package org.techit.week4.day2;
 
 public class MultiplicationTable2To4 {
+    public MultiplicationTable2To4(String multipleSym) {
+        this.multipleSym = multipleSym;
+    }
 
+    private String multipleSym = "*";
     void printMultip(int num){
         for (int i = 1; i <= 9; i++) {
-            System.out.printf("%d * %d = %d\n", num, i, num * i);
+            System.out.printf("%d %s %d = %d\n", num,multipleSym, i, num * i);
         }
         System.out.printf("-----------\n");
     }
     public static void main(String[] args) {
-        MultiplicationTable2To4 mu = new MultiplicationTable2To4();
+        MultiplicationTable2To4 mu = new MultiplicationTable2To4("x");
+        MultiplicationTable2To4 mt2 = new MultiplicationTable2To4("*");
         mu.printMultip(2);
-        mu.printMultip(3);
+        mt2.printMultip(3);
         mu.printMultip(4);
-        mu.printMultip(6);
+        mt2.printMultip(6);
 
     }
 }
