e0dd856165e2b7b53e8dae2b19cac52507f42cb3
Unyeong Kang <dnsdud426@naver.com>
2023-05-11 09:22:18 +0900

    feat : 리스트 연습

---
+++ src/main/java/org/techit/week4/day4/ListEx.java
@@ -0,0 +1,17 @@
+package org.techit.week4.day4;
+
+import java.util.*;
+
+public class ListEx {
+    public static void main(String[] args) {
+        List l1 = new ArrayList();
+        l1.add("Hello");
+        l1.add(1);
+        System.out.println(l1.get(0));
+        System.out.println(l1.get(1));
+        System.out.println(l1.isEmpty());
+        System.out.println(l1.size());
+        l1.remove(0);
+        System.out.println(l1.get(0));
+    }
+}
